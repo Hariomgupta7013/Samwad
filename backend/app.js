825994aae21548f63aec673dbb369767957b3edc
Hariom Gupta <hariomgupta8057@gmail.com>
2026-04-11 04:34:48 +0530

    update login

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,10 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
-import { connectToSocket } from "./controllers/socketManager.js";
+import { connectToSocket } from "./src/controllers/socketManager.js";
 import path from "path"
 import cors from "cors";
-import userRoutes from "./routes/users_routes.js"
+import userRoutes from "./src/routes/users_routes.js"
 
 const app = express();
 const server = createServer(app);
